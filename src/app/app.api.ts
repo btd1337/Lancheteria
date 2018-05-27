cf81364273caee02d5a4122f747c21100ae37de5
btd1337 <helder.bertoldo@ice.ufjf.br>
2018-05-27 02:26:03 -0300

    0.7.3
    - Update api url

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,3 +1,2 @@
-/* export const LANCHETERIA_API = 'http://localhost:3000'; */
-
-export const LANCHETERIA_API = 'http://localhost:3003/api';
+/* export const LANCHETERIA_API = 'http://localhost:3003/api'; */
+export const LANCHETERIA_API = 'https://lancheteria-api.herokuapp.com/api';
